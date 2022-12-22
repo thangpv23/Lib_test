7ffaa571382a34cebd67c61210875f3d1148a030
Phạm Văn Thắng <thangpv2303@gmail.com>
2022-12-22 14:15:33 +0700

    remove require redundant

--- mmdetection/setup.py
+++ mmdetection/setup.py
@@ -210,13 +210,11 @@ if __name__ == '__main__':
         ],
         license='Apache License 2.0',
         install_requires=parse_requirements('requirements/runtime.txt'),
-        extras_require={
-            'all': parse_requirements('requirements.txt'),
-            'tests': parse_requirements('requirements/tests.txt'),
-            'build': parse_requirements('requirements/build.txt'),
-            'optional': parse_requirements('requirements/optional.txt'),
-            'mim': parse_requirements('requirements/mminstall.txt'),
-        },
+        # extras_require={
+        #     'all': parse_requirements('requirements.txt'),
+        #     'build': parse_requirements('requirements/build.txt'),
+        #     'mim': parse_requirements('requirements/mminstall.txt'),
+        # },
         ext_modules=[],
         cmdclass={'build_ext': BuildExtension},
         zip_safe=False)
