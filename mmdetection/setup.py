a8a283443ae1c4d34efbb0946806f9c6ad5826d8
thangpv23 <53090278+thangpv23@users.noreply.github.com>
2022-12-22 15:01:06 +0700

    Update setup.py

--- mmdetection/setup.py
+++ mmdetection/setup.py
@@ -19,13 +19,30 @@ def readme():
     return content
 
 
-version_file = 'mmdetection/card_segment/mmdet/version.py'
+
+__version__ = '2.25.1'
+short_version = __version__
 
 
-def get_version():
-    with open(version_file, 'r') as f:
-        exec(compile(f.read(), version_file, 'exec'))
-    return locals()['__version__']
+def parse_version_info(version_str):
+    version_info = []
+    for x in version_str.split('.'):
+        if x.isdigit():
+            version_info.append(int(x))
+        elif x.find('rc') != -1:
+            patch_version = x.split('rc')
+            version_info.append(int(patch_version[0]))
+            version_info.append(f'rc{patch_version[1]}')
+    return tuple(version_info)
+
+
+version_info = parse_version_info(__version__)
+
+
+# def get_version():
+#     with open(version_file, 'r') as f:
+#         exec(compile(f.read(), version_file, 'exec'))
+#     return locals()['__version__']
 
 
 def make_cuda_ext(name, module, sources, sources_cuda=[]):
@@ -187,7 +204,7 @@ if __name__ == '__main__':
     add_mim_extension()
     setup(
         name='CardSegment',
-        version=get_version(),
+        version=version_info,
         description='OpenMMLab Detection Toolbox and Benchmark',
         long_description=readme(),
         long_description_content_type='text/markdown',
