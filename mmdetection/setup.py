92c84c7817a5e6c13e43fe624dc5950304b90584
thangpv23 <53090278+thangpv23@users.noreply.github.com>
2022-12-22 15:17:40 +0700

    Update setup.py

--- mmdetection/setup.py
+++ mmdetection/setup.py
@@ -8,6 +8,8 @@ import sys
 import warnings
 from setuptools import find_packages, setup, find_namespace_packages
 
+import os
+
 import torch
 from torch.utils.cpp_extension import (BuildExtension, CppExtension,
                                        CUDAExtension)
@@ -19,30 +21,14 @@ def readme():
     return content
 
 
-
-__version__ = '2.25.1'
-short_version = __version__
-
-
-def parse_version_info(version_str):
-    version_info = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            version_info.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            version_info.append(int(patch_version[0]))
-            version_info.append(f'rc{patch_version[1]}')
-    return tuple(version_info)
+cur_path = os.path.dirname(__file__)
+version_file =  cur_path+"/card_segment/mmdet/version.py"
 
 
-version_info = parse_version_info(__version__)
-
-
-# def get_version():
-#     with open(version_file, 'r') as f:
-#         exec(compile(f.read(), version_file, 'exec'))
-#     return locals()['__version__']
+def get_version():
+    with open(version_file, 'r') as f:
+        exec(compile(f.read(), version_file, 'exec'))
+    return locals()['__version__']
 
 
 def make_cuda_ext(name, module, sources, sources_cuda=[]):
@@ -204,7 +190,7 @@ if __name__ == '__main__':
     add_mim_extension()
     setup(
         name='CardSegment',
-        version=version_info,
+        version=get_version(),
         description='OpenMMLab Detection Toolbox and Benchmark',
         long_description=readme(),
         long_description_content_type='text/markdown',
